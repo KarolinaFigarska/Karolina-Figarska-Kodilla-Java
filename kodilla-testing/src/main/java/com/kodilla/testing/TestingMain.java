79b717ef519f56785f3195784c8c18dc20d91448
Karolina Figarska <figarska.karolina@gmail.com>
2021-02-10 13:23:57 +0100

    6.2 Added test for class Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator(10,5);
+
+        //pierwszy test jednostkowy
+        if (calculator.add() == 15) {
+            System.out.println("FIRST TEST OK!");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+        //drugi test jednostkowy
+        if (calculator.substract() == 5) {
+            System.out.println("SECOND TEST OK!");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
