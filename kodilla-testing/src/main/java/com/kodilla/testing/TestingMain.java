d4009f2f11f184eb1a5d6a430c35574fccbb5b93
Karolina Figarska <figarska.karolina@gmail.com>
2021-02-11 09:34:39 +0100

    6.2 Removing fields from a class and passing data straight to methods

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,17 +4,17 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(10,5);
+        Calculator calculator = new Calculator();
 
         //pierwszy test jednostkowy
-        if (calculator.add() == 15) {
+        if (calculator.add(10,5) == 15) {
             System.out.println("FIRST TEST OK!");
         } else {
             System.out.println("ERROR!");
         }
 
         //drugi test jednostkowy
-        if (calculator.substract() == 5) {
+        if (calculator.substract(10,4) == 5) {
             System.out.println("SECOND TEST OK!");
         } else {
             System.out.println("Error!");
