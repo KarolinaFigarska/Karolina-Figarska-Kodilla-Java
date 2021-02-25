826bd6b106e35e588ed90e2e77562c1b5286aaa3
Karolina Figarska <figarska.karolina@gmail.com>
2021-02-25 11:44:34 +0100

    7.3 Using LocalDate.now

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,8 +3,10 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 public class StreamMain {
 
     public static void main(String[] args) {
@@ -12,15 +14,16 @@ public class StreamMain {
         Forum forum = new Forum();
         Map<Integer, ForumUser> theMapOfForumUser = forum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.currentAge(2021) >= 20)
+                .filter(forumUser -> forumUser.getBirthDate().isBefore(LocalDate.now().minusYears(20)))
                 .filter((forumUser -> forumUser.getNumberOfPosts() >= 1))
                 .collect(Collectors.toMap(ForumUser::getIdNumber, forumUser -> forumUser));
 
         theMapOfForumUser.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
-    }
+   }
 }
+
 
 
 
