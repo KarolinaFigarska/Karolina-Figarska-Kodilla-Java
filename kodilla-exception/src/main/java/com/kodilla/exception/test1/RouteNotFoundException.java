57675833cfd42892edfd611334a4db498ab7465f
Karolina Figarska <figarska.karolina@gmail.com>
2021-03-02 14:12:53 +0100

    8.4 Exceptions

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test1/RouteNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kodilla.exception.test1;
+
+public class RouteNotFoundException extends Exception {
+    public RouteNotFoundException(final String message) {
+       super(message);
+    }
+}
