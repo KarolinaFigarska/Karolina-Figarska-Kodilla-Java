37f1446a0066950e4d6ba7c610e3bbe4a65d4c59
Karolina Figarska <figarska.karolina@gmail.com>
2021-03-31 13:40:28 +0200

    13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Main.java
@@ -1,15 +1,59 @@
 package com.kodilla.good.patterns;
 
-import com.kodilla.good.patterns.challenges.MovieStore;
+import com.kodilla.good.patterns.challenges.Food2Door.*;
+import com.kodilla.good.patterns.challenges.flights.Flight;
+import com.kodilla.good.patterns.challenges.flights.FlightBase;
+import com.kodilla.good.patterns.challenges.flights.FlightSearch;
+
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-        String allMovies = MovieStore.getMovies().entrySet().stream()
-                .flatMap(stringListEntry -> stringListEntry.getValue().stream())
-                .collect(Collectors.joining("!", " ", " "));
 
-        System.out.println(allMovies);
+        FlightSearch.flightFrom(Flight.getKATOWICE());
+        FlightSearch.flightFrom(Flight.getBYDGOSZCZ());
+
+        FlightSearch.flightTo(Flight.getKATOWICE());
+        FlightSearch.flightTo(Flight.getWROCLAW());
+
+        FlightSearch.flightTransfer(Flight.getKATOWICE(),Flight.getWROCLAW());
+///////////////////////////////////////////////////
+//        Supply supply = new Supply();
+//        supply.deliveryOfGoods();
+//        supply.printDeliveryOfGoods();
+//
+
+
+///////////////////
+        //  String allMovies = MovieStore.getMovies().entrySet().stream()
+        //         .flatMap(stringListEntry -> stringListEntry.getValue().stream())
+        //        .collect(Collectors.joining("!", " ", " "));
+
+        // System.out.println(allMovies);
+////////////////
+//        CreateUser createUser = new CreateUser();
+//
+//        UserRecord donaldRecord = createUser.createUserDonald();
+//        createUser.setOrderDate(donaldRecord);
+//        createUser.setPaymentDate(donaldRecord);
+//
+//        ProductOrderService productOrderService = new ProductOrderService(
+//                new MailService(), new ShoesOrderService(), new OrderRepository());
+//        productOrderService.process(donaldRecord);
+//
+//        UserRecord joeRecord = createUser.createUserJoe();
+//        createUser.setOrderDate(joeRecord);
+//        createUser.setPaymentDate(joeRecord);
+//
+//        ProductOrderService productOrderService1 = new ProductOrderService(
+//                new MailService(), new GameOrderService(), new OrderRepository());
+//        productOrderService1.process(joeRecord);
+//
+//        ProductOrderService productOrderService2 = new ProductOrderService(
+//                new MailService(), new ToysOrderService(), new OrderRepository());
+//        productOrderService2.process(joeRecord);
     }
 }
