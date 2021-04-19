f5d085452957ecf9737949dcb4a378482d981b19
Karolina Figarska <figarska.karolina@gmail.com>
2021-04-19 11:38:11 +0200

    15.5 Builder

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Burgers.java
@@ -0,0 +1,7 @@
+package com.kodilla.patterns.builder.bigmac;
+
+public enum Burgers {
+    BEEF_CHOPS_1,
+    BEEF_CHOPS_2,
+    BEEF_CHOPS_3,
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+
+    @Test
+    void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun(Bun.WITH_SESAME)
+                .burgers(Burgers.BEEF_CHOPS_2)
+                .sauce(Sauce.STANDARD)
+                .ingredient(Ingredients.BACON)
+                .ingredient(Ingredients.CHEESE)
+                .build();
+
+        //When
+        int quantityOfIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(2, quantityOfIngredients);
+
+    }
+}
