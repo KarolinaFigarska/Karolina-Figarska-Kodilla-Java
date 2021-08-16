e62e472dd41eb11d5740cd538525a19e61a336ff
Karolina Figarska <figarska.karolina@gmail.com>
2021-08-16 18:41:53 +0200

    Exception update

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,10 +9,31 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    //Dotyczy modułu 8.1-8.2
+
+//    public void readFile() throws IOException {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        // This line is not used because there is an issue with path
+//        File file = new File(classLoader.getResource("names.txt").getFile());
+//        // Instead of file.getPath() I use fix path
+//        try (Stream<String> fileLines = Files.lines(Paths.get("kodilla-exception/src/main/resources/names.txt"))) {
+//            // file.getPath() - points to kodilla-exception\build\resources\main\names.txt
+//            // I am not sure why this path is incorrect
+//            // try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+//            fileLines.forEach(System.out::println);
+//        } catch (IOException e) {
+//            System.out.println("Oh no! Something went wrong! Error:" + e);
+//        } finally {
+//            System.out.println("I am gonna be here... always!");
+//        }
+//    }
+
+    //Dotyczy modułu 8.3
+
+        public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         // This line is not used because there is an issue with path
-        File file = new File(classLoader.getResource("names.txt").getFile());
+        //File file = new File(classLoader.getResource("names.txt").getFile());
         // Instead of file.getPath() I use fix path
         try (Stream<String> fileLines = Files.lines(Paths.get("kodilla-exception/src/main/resources/names.txt"))) {
             // file.getPath() - points to kodilla-exception\build\resources\main\names.txt
@@ -20,7 +41,7 @@ public class FileReader {
             // try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("Oh no! Something went wrong! Error:" + e);
+            throw  new FileReaderException();
         } finally {
             System.out.println("I am gonna be here... always!");
         }
