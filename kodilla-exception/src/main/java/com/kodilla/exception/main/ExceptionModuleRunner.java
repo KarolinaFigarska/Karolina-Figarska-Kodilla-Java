45fc95434d4a2668d46a0bb523edafeb23dfce9e
Karolina Figarska <figarska.karolina@gmail.com>
2021-08-16 18:41:53 +0200

    Exception update

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,22 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
+        //Dotyczy modułu 8.1-8.2
+        //FileReader fileReader = new FileReader();
+        //fileReader.readFile();
+
+        //Dotyczy modułu 8.3
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Problem while reading a file!");
+        }
     }
 }
