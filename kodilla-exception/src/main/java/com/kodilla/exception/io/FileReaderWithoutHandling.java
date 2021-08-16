543410c0fb71980afd866cd4899c204d14491009
Karolina Figarska <figarska.karolina@gmail.com>
2021-08-16 18:41:53 +0200

    Exception update

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
@@ -3,16 +3,20 @@ package com.kodilla.exception.io;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReaderWithoutHandling {
 
-//     public void readFile() throws IOException {
-//         ClassLoader classLoader = getClass().getClassLoader();
-//         File file = new File(classLoader.getResource("names.txt").getFile());
-//         Stream<String> fileLines = Files.lines(Paths.get(file.getPath()));
-//         fileLines.forEach(System.out::println);
-//     }
+    public void readFile() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        // This line is not used because there is an issue with path
+        File file = new File(classLoader.getResource("names.txt").getFile());
+        // Instead of file.getPath() I use fix path
+        Stream<String> fileLines = Files.lines(Paths.get("kodilla-exception/src/main/resources/names.txt"));
+            // file.getPath() - points to kodilla-exception\build\resources\main\names.txt
+            // I am not sure why this path is incorrect
+            // try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+            fileLines.forEach(System.out::println);
+    }
 }
