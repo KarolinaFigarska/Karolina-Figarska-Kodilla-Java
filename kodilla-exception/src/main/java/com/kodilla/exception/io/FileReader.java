1b62897e2aa9a5897938f003a1b801bafd425e3a
Karolina Figarska <figarska.karolina@gmail.com>
2021-08-16 11:51:59 +0200

    Path issue

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -3,6 +3,7 @@ package com.kodilla.exception.io;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
@@ -10,9 +11,13 @@ public class FileReader {
 
     public void readFile() {
         ClassLoader classLoader = getClass().getClassLoader();
+        // This line is not used because there is an issue with path
         File file = new File(classLoader.getResource("names.txt").getFile());
-
-        try (Stream<String> fileLines = Files.lines(Paths.get("tego-pliku-nie-ma.txt"))) {
+        // Instead of file.getPath() I use fix path
+        try (Stream<String> fileLines = Files.lines(Paths.get("kodilla-exception/src/main/resources/names.txt"))) {
+            // file.getPath() - points to kodilla-exception\build\resources\main\names.txt
+            // I am not sure why this path is incorrect
+            // try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
             System.out.println("Oh no! Something went wrong! Error:" + e);
